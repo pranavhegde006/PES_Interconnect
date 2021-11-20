e5868d86ea4b1ad896d5c74cbf3ecb5a60711c28
pranavhegde006 <pranavhegde06@gmail.com>
2021-11-20 10:50:10 +0530

    add login

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, send_from_directory
 import psycopg2
 import psycopg2.extras
 from flask_sqlalchemy import SQLAlchemy
-import os
-
-from dotenv import load_dotenv
-load_dotenv()
 
 app = Flask(__name__)
 
-DB_HOST = "localhost"
-DB_NAME = "pes_interconnect2"
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASS']
-
-conn = psycopg2.connect(dbname = DB_NAME, user = DB_USER, password = DB_PASSWORD, host = DB_HOST)
-
-
 @app.route('/')
+def login_page():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def login():
+    try: 
+        DB_HOST = "localhost"
+        DB_NAME = "pes_interconnect2"
+        DB_USER = request.form['username']
+        DB_PASSWORD = request.form['password']
+        global conn
+        conn = psycopg2.connect(dbname = DB_NAME, user = DB_USER, password = DB_PASSWORD, host = DB_HOST)
+        return redirect('/home')
+    except:
+        return render_template('error.html')
+
+
+
+@app.route('/home', methods=['GET'])
 def index():
+    # try:
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     s = "SELECT * FROM student"
     cur.execute(s)
     rows = cur.fetchall()
-    print(rows)
 
     return rows[0]
 
